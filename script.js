aa4bbb94a6569e539e141e728dfeffda193f1c96
Eduardo Mantz <eduardomantz2112@gmail.com>
2020-08-16 00:58:43 -0300

    verção 1.0.6
    finalizado o front-end

--- script.js
+++ script.js
@@ -54,3 +54,30 @@ function validate() {
   return false
  } 
 }
+
+const ulSquares = document.querySelector("ul.squares");
+
+for (let i = 0; i < 25; i++) {
+  const li = document.createElement("li");
+
+  const random = (min, max) => Math.random() * (max - min) + min;
+
+  const size = Math.floor(random(10, 120));
+  const position = random(1, 99);
+  const delay = random(5, 0.1);
+  const duration = random(24, 12);
+  const boderRadius = random(5, 50);
+
+  li.style.width = `${size}px`;
+  li.style.height = `${size}px`;
+  li.style.bottom = `-${size}px`;
+  li.style.borderRadius = `${boderRadius}px`;
+
+  li.style.left = `${position}%`;
+
+  li.style.animationDelay = `${delay}s`;
+  li.style.animationDuration = `${duration}s`;
+  li.style.animationTimingFunction = `cubic-bezier(${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()})`;
+
+  ulSquares.appendChild(li);
+}
