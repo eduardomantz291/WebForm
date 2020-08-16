0630f4aa08a2482c53daa508899c4b9bfff8bf8d
Eduardo Mantz <eduardomantz2112@gmail.com>
2020-08-16 00:01:18 -0300

    versão 1.0.4
    Iniciem o script.js e já crie umas variáveis.

---
+++ script.js
@@ -0,0 +1,8 @@
+const btnLogin = document.querySelector(".btn-login");
+const form = document.querySelector("form");
+const email = document.querySelector("#login-email");
+const password = document.querySelector("#login-password");
+
+function validate() {
+ if (email.value == "") {
+}
