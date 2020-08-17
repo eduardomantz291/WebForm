01557f830a243d5d8096a01bcf214529f9f48ceb
Eduardo Mantz <eduardomantz2112@gmail.com>
2020-08-17 07:53:14 -0300

    versção 1.1.4
    primeira rota e baixei o nodemon.

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const server = express();
 
+server.get("/", (req, res) => {
+  res.send("Hello!")
+})
 
-
-server.listen(5500)
+server.listen(5500, () => {
+  console.log("Servidor rodando na url http://127.0.0.1:5500")
+});
