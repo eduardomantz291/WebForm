7243d4d6f10d9cf5ad1eddcd38584d01c136fa8b
Eduardo Mantz <eduardomantz2112@gmail.com>
2020-08-18 01:31:27 -0300

    versão 1.1.6
    Servidor rodando e com os arquivos estáticos.

--- src/server.js
+++ src/server.js
@@ -1,14 +1,23 @@
+function Index(req, res) {
+  return res.render("index.html");
+}
+
+function Register(req, res) {
+   return res.render("register.html");
+}
+
 const express = require("express");
 const server = express();
 
-server.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
+const nunjucks = require("nunjucks");
+nunjucks.configure("src/views", {
+  express: server,
+  noCache: true,
 })
 
-server.get("/register", (req, res) => {
-  res.sendFile(__dirname + "/views/register.html");
-})
-
-server.listen(5500, () => {
+server.use(express.static("public"))
+.get("/", Index)
+.get("/register", Register)
+.listen(5500, () => {
   console.log("Servidor rodando!");
 })
